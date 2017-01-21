3b5a0f208d59b873f2100044956cdaff67ab0ef3
Aniruddh Iyengar <aniruddh.iyengar202@gmail.com>
2017-01-21 02:50:56 -0500

    Initial Commit

---
+++ sphere/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sphere
+//
+//  Created by Aniruddh Iyengar on 1/21/17.
+//  Copyright © 2017 Aniruddh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sphere/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sphere
+//
+//  Created by Aniruddh Iyengar on 1/21/17.
+//  Copyright © 2017 Aniruddh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
